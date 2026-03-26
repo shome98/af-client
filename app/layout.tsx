8a426afdbc9b4040816f2d4f4bed2722d7b62d13
Sowparna Shome <soupornoshome@gmail.com>
2026-03-27 00:07:01 +0530

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Montserrat } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import { cn } from "@/lib/utils";
+
+const montserrat = Montserrat({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +29,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", montserrat.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
