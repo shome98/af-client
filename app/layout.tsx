b2f0de5b6880e85db3925a5360f0ad914114b210
Sowparna Shome <soupornoshome@gmail.com>
2026-03-27 00:22:42 +0530

    chore: reformat + dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,27 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Montserrat } from "next/font/google";
+import type {Metadata} from "next";
+import {Montserrat} from "next/font/google";
 import "./globals.css";
 import React from "react";
-import { cn } from "@/lib/utils";
+import {cn} from "@/lib/utils";
 
-const montserrat = Montserrat({subsets:['latin'],variable:'--font-sans'});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const montserrat = Montserrat({subsets: ['latin'], variable: '--font-sans'});
 
 export const metadata: Metadata = {
-  title: "Api Hub",
-  description: "get rest apis in minutes!!!😊",
+    title: "Api Hub",
+    description: "get rest apis in minutes!!!😊",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html
-      lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", montserrat.variable)}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
+    return (
+        <html
+            lang="en"
+            className={cn("h-full", "antialiased", "font-sans", montserrat.variable, "dark")}
+        >
+        <body className="min-h-full flex flex-col">{children}</body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-      <div>
-        <h1 className="text-3xl text-center mt-10">Welcome!</h1>
-      </div>
-   );
+    return (
+        <div>
+            <h1 className="text-3xl text-center mt-10">Welcome!</h1>
+        </div>
+    );
 }
