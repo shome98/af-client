849eb5f719242281ada57f1cb85fb20e1c9b5bcd
Sowparna Shome <soupornoshome@gmail.com>
2026-04-12 23:38:25 +0530

    chore: add icon and remove assests

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -1,8 +1,15 @@
 import { APP } from '@/constants/landing.constant';
+import Image from 'next/image';
 import React from 'react';
+import icon from '@/app/icon.png';
 
 const Logo = () => {
-  return <div>{APP.name}</div>;
+  return (
+    <div className='flex gap-2'>
+      <Image src={icon} alt="site logo image" width={20} height={20} />
+      <span>{APP.name}</span>
+    </div>
+  );
 };
 
 export default Logo;
